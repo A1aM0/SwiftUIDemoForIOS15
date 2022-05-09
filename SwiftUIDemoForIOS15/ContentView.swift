6ec5eafb4507a1d21a2751fffc99893602548d89
Songhao Yang <yoyoyanghao@126.com>
2022-05-09 23:12:45 +0800

    Initial Commit

---
+++ SwiftUIDemoForIOS15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemoForIOS15
+//
+//  Created by 杨松皓 on 2022/5/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemoForIOS15/SwiftUIDemoForIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoForIOS15App.swift
+//  SwiftUIDemoForIOS15
+//
+//  Created by 杨松皓 on 2022/5/9.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoForIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
