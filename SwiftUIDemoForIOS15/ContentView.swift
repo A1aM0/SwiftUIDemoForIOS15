79be7adc06bed9ea889e88cef8d91acabec92aa7
Songhao Yang <yoyoyanghao@126.com>
2022-05-10 00:25:08 +0800

    手动添加组件到画布

--- SwiftUIDemoForIOS15/ContentView.swift
+++ SwiftUIDemoForIOS15/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 8) {
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20)
+            Text("SwiftUI for iOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque nec nam aliquam sem.")
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .font(.footnote)
+            Spacer()
+        }
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
+        .padding(.all, 20)
+        .shadow(radius: 20)
+        .frame(height: 350)
+        .cornerRadius(30)
     }
 }
 
