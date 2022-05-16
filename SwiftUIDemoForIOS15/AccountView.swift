178d0fdf9b3634997c194ec1e89ab920281a909a
Songhao Yang <yoyoyanghao@126.com>
2022-05-17 00:26:01 +0800

    画个人页面

--- SwiftUIDemoForIOS15/AccountView.swift
+++ SwiftUIDemoForIOS15/AccountView.swift
@@ -11,10 +11,35 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List{
+                VStack(spacing: 8) {
+                    Image(systemName: "person.crop.circle.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(
+                            Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundColor(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100)
+                    )
+                    Text("A1aM0")
+                        .font(.title.weight(.semibold))
+                    HStack {
+                        Image(systemName: "location")
+                            .imageScale(.large)
+                        Text("China")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity)
                 Section {
-                    Text("settings")
-                    Text("Billing")
-                    Text("Help")
+                    Label("settings", systemImage: "gear")
+                    Label("Billing", systemImage: "creditcard")
+                    Label("Help", systemImage: "questionmark")
                 }
                 .listRowSeparatorTint(.red)
                 .listRowSeparator(.hidden)
